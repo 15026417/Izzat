cc0312fc29bb5913d9d8ce701599c87804cb8827
Naveed_khan <naveedk2015@namal.edu.pk>
2016-11-15 18:31:06 -0800

    Izzat Updated File

---
+++ Helloworld.java
@@ -0,0 +1,63 @@
+//public class Helloworld
+//{
+  //public static void main(String[] args)
+  //{
+  // int    a=15;
+  //  if(a==15)
+ //   {
+//      System.out.print("dsds");
+ //   }
+//  }
+//}
+//Q1 HAS NO ERROR.
+
+
+//public class Helloworld
+//{
+  //public static void main(String[] args)
+  //{
+   
+     // System.out.print(100/0);
+    
+  //}
+//}
+
+//Q2 we can not devide any number by zero.it compile but not run.IT HAS RUN TIME ERROR.
+
+
+
+//public class Helloworld
+//{
+  //public static void main(String[] args)
+  //{
+   
+  //    Sytem.out.print(100/100);
+    
+ // }
+//}
+//Q3.There is a syntax error.s is missing in system.it Should be System.out.print(100/100);
+
+//public class Helloworld
+//{
+ // public static void main(String[] args)
+  //{
+   //  int a,b=15;
+    // System.out.print(a+"   "+b);
+    
+  //}
+//}
+//Q4. it has Syntax error.becase a not initilized. it should be  a=integer value;
+
+public class Helloworld
+{
+  public static void main(String[] args)
+  {
+     int a=15;
+      int b=15;
+      c=a+b;
+     System.out.print(c);
+    
+  }
+}
+
+//Q5  c is not declare,  first we should declare c then it should be int c=a+b;
